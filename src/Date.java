efddfdb5bc830a0038895cb2e242551e9f60ae90
Panayotis Matsinopoulos <Panayotis Matsinopoulos panayotis@matsinopoulos.gr>
2017-08-19 14:57:04 +0300

    Initial Commit

---
+++ src/Date.java
@@ -0,0 +1,16 @@
+public class Date {
+	public static void main(String[] args) {
+		String day = "Friday";
+		int date = 13;
+		String month = "August";
+		int year = 2018;
+		System.out.println(day);
+		System.out.println(date);
+		System.out.println(month);
+		System.out.println(year);
+		System.out.println("American format:");
+		System.out.println(day + ", " + month + " " + date + ", " + year);
+		System.out.println("European format:");
+		System.out.println(day + " " + date + " " + month + " " + year);
+	}
+}
